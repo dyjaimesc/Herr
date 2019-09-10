766034d7f8570ed9a5b3ec6b3486fcb145dcc1bd
Dany Jaimes <dyjaimesc@unal.edu.co>
2019-09-10 10:25:41 -0500

    Arregla la funcion para encontrar primos

--- 2019-09-06/primos.cpp
+++ 2019-09-06/primos.cpp
@@ -2,17 +2,22 @@
 #include<cstdio>
 
 int primos(int);
-int suma(int);
+//int suma(int);
 
 int main(void)
 {
-  int inicial=50;
-  int final=100;
+  int inicial=5;
+  int final=10;
   int salida=0;
     for(int i=inicial;i<=final;i++)
       {
 	if(primos(i)==1)
-	  {salida=salida+i;}
+	  {
+	    salida=salida+i;
+	    std::cout<<i<<"\n";
+	  }
+
+	
       }
     std::cout<<salida<<"\n";
   return 0;
@@ -22,9 +27,9 @@ int primos(int numero)
 {
    for(int i=2;i<=numero/2;i++)
     {
-      if(numero%2==0) return 0;
+      if(numero%i==0) return 0;
     }
-  return 1; 
+   return 1; //Retorna 1 cuando el numero es primo
 }
 
 
