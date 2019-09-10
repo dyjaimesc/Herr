d3680bc7343e665b25a04a28856a3d3bf8676a58
Dany Jaimes <dyjaimesc@unal.edu.co>
2019-09-10 11:51:17 -0500

    Programa que realiza la suma de los numeros primos desde un valor inicial hasta un valor final.

--- 2019-09-06/primos.cpp
+++ 2019-09-06/primos.cpp
@@ -2,24 +2,27 @@
 #include<cstdio>
 
 int primos(int);
+int suma_primos(int ,int );
 //int suma(int);
 
 int main(void)
 {
-  int inicial=5;
-  int final=10;
-  int salida=0;
-    for(int i=inicial;i<=final;i++)
-      {
-	if(primos(i)==1)
-	  {
-	    salida=salida+i;
-	    std::cout<<i<<"\n";
-	  }
+  int valor_inicial=50;
+  int valor_final=100;
+  std::cout<< suma_primos(valor_inicial,valor_final)<<"\n" ;
 
-	
-      }
-    std::cout<<salida<<"\n";
+  // int inicial=5;
+  // int final=10;
+  // int salida=0;
+  //   for(int i=inicial;i<=final;i++)
+  //     {
+  // 	if(primos(i)==1)
+  // 	  {
+  // 	    salida=salida+i;
+  // 	    std::cout<<i<<"\n";
+  // 	  }
+  //     }
+  //   std::cout<<salida<<"\n";
   return 0;
 }
 
@@ -30,6 +33,20 @@ int primos(int numero)
       if(numero%i==0) return 0;
     }
    return 1; //Retorna 1 cuando el numero es primo
+}
+
+int suma_primos(int ini, int fin)
+{
+  int out=0;
+  for(int i=ini;i<=fin;i++)
+    {
+      if(primos(i)==1)
+	  {
+	    out=out+i;
+	    std::cout<<i<<"\n";
+	  }
+    }
+  return out;
 }
 
 
