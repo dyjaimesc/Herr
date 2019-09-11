080e51445706da4be4899f67043ce3f9e7a9d15a
Dany Jaimes <dyjaimesc@unal.edu.co>
2019-09-11 12:05:56 -0500

    Programa overflow

--- Varios/overflow.cpp
+++ Varios/overflow.cpp
@@ -1,6 +1,20 @@
-#include<iostream>
+#include <iostream>
+
+int fsuma(int);
 
 int main()
-{
+{ int variable=0;
+  variable=fsuma(variable);
+  std::cout<<"el mayor entero es "<<variable<< " \n";
   return 0;
+}
+
+int fsuma(int n)
+{int x=1,y=0;
+  
+  while(x>0)
+    { y=x;
+      x+=1;
+    }
+  return y;
 }
