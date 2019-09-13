8b58fd7a9197cd39de52ef95b2ee8a9cf639bd1f
Dany Jaimes <dyjaimesc@unal.edu.co>
2019-09-13 12:28:15 -0500

    Programa que cuenta los numeros primos y los suma

--- tarea1/contador_primos.cpp
+++ tarea1/contador_primos.cpp
@@ -1,24 +1,14 @@
 #include<cstdio>
 
-int primos(int);
+int primos(int);//funcion que regresa 1 si el numero ingresado es primo y 0 en otro caso
+int suma_primos(int,int);//funcion que suma cuantos primos hay entre un valor inicial y un valor final, suma el valor de todos estos primos; los parametros de entrada son los valores inicial y final de los primos.
 
 int main(void)
 {
   int inicial=500;
   int final=12400;
-  int contador=0;
-  int suma=0;
 
-  for(int i=inicial;i<=final;i++)
-    {
-      if(primos(i)==1)
-	{
-	  // printf("%d \n",i);
-	  contador=++contador;
-	  suma+=i;
-	}
-    }
-  printf("Hay %d numeros primos entre %d y %d; su suma es %d \n", contador,inicial, final,suma);
+  suma_primos(inicial,final);
   
   return 0;
 }
@@ -33,3 +23,20 @@ int primos(int n)
   return 1;
 }
 
+int suma_primos(int ini, int fin)
+{
+  
+  int contador=0;
+  int suma=0;
+
+  for(int i=ini;i<=fin;i++)
+    {
+      if(primos(i)==1)
+	{
+	  // printf("%d \n",i);
+	  contador=++contador;
+	  suma+=i;
+	}
+    }
+  printf("Hay %d numeros primos entre %d y %d; su suma es %d \n", contador,ini, fin,suma);
+}
