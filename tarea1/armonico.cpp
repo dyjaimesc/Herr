d3cbd897b264f1cb12a362e6c65945cf1b583375
Dany Jaimes <dyjaimesc@unal.edu.co>
2019-09-17 07:28:16 -0500

    Punto 2 de la primer tarea

--- tarea1/armonico.cpp
+++ tarea1/armonico.cpp
@@ -1,24 +1,29 @@
 #include<cstdio>
+#include<fstream>
 
 int suma_armonica(int,int);
 
 int main(void)
 {
   int ini=1;
-  int fin=10;
+  int fin=1000;
   suma_armonica(ini,fin);
   
   return 0;
 }
 
-int suma_armonica(int ini,int fin)
+int suma_armonica(int ii,int end)
 {
+  std::ofstream fout("datos.txt");
   double suma=0.0;
   double div=0.0;
-  for(int i=ini;i<=fin;i++);
+
+  for(int j=ii;j<=end;j++)
   {
-    div=1.0/i;
-    suma+=div;
-    printf("%d %6.5f",i,suma);
+    
+     div=1.0/j;
+     suma+=div;
+     fout<<j<<"\t"<<suma<<"\n";
+     // printf("%3d %8.5f\n",j,suma);
   }
 }
