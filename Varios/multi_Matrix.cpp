2848322eae93422d834ef6533b91b26fda494d74
Dany Jaimes <dyjaimesc@unal.edu.co>
2019-11-07 08:35:55 -0500

    Documentos realizados multiplicacion matriz

--- Varios/multi_Matrix.cpp
+++ Varios/multi_Matrix.cpp
@@ -3,11 +3,15 @@
 #include<cmath>
 
 const int N=3;
-double imprimir(const double *M1);
+double imprimir(double *M1);
+double multipli_mtx(double *M1,double *M2, double *P);
+double transpuesta(double *M1,double *M2);
 
 int main()
 {
-  double *A= new double [N*N], *B=new double[N*N];
+  printf("Hola\n");
+
+  double *A= new double [N*N], *B=new double[N*N],*C=new double[N*N], *AT=new double[N*N];
 
   //inicializo la matriz
 
@@ -18,28 +22,86 @@ int main()
 	    A[i*N+j]=i+j+1.0;
 	    //printf("%d %d %5.3f %f\n",i,j,A[i*N+j],i+j+1.0);
 		   
-	    B[i*N+j]=1.0*N-i-j;
+	    B[i*N+j]=1.0*N*N-i-j-1.0;
+	    
 	    
 	  }
     }
-
- 
-  
-  imprimir(A);
+ imprimir(A);
+  imprimir(B);
+  multipli_mtx(A,B,C);
+  imprimir(C);
+  transpuesta(C,AT);
+  imprimir(AT);
   
   return 0;
 }
 
-double imprimir(const double *M1)
+
+double imprimir( double *M1)
 {
  
   for(int i=0;i<N;i++)
     {
         for(int j=0;j<N;j++)
 	  {
-	    printf("%5.3f ",M1[i+j]);
+	    printf("%5.3f ",M1[i*N+j]);
 	  }
 	printf("\n");
     }
+  printf("\n");
   return 0;
+}
+
+
+double multipli_mtx(double *M1,double *M2, double *P)
+{
+  double sum=0.0;
+
+  imprimir(M1);
+  imprimir(M2);
+  
+  for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+	  {
+	    for(int k=0;k<N;k++)
+	      {
+		sum+=M1[i*N+k]*M2[k*N+j];
+	      }
+	    P[i*N+j]=sum;
+	    sum=0.0;
+	  }
+
+    }
+  
+
+  /*   for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+	  {
+	    P[i*N+j]=1.0;
+	    printf("%d %d %5.3f %f\n",i,j,P[i*N+j],i+j+1.0);
+		   
+	    
+	    
+	  }
+	  }*/
+
+  return 0;
+}
+
+double transpuesta(double *M1,double *M2)
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+	  {
+	    M2[i*N+j]=M1[j*N+i];
+	   
+		   
+	    
+	    
+	  }
+	  }
 }
