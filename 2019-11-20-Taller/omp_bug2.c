16734a4d638161378d7faee9dd8afb0cae376055
Slackware Live User <live@darkstar.example.net>
2019-12-04 23:05:17 -0800

    Tarea 2 y respectivos makefiles

--- 2019-11-20-Taller/omp_bug2.c
+++ 2019-11-20-Taller/omp_bug2.c
@@ -11,12 +11,14 @@
 
 int main (int argc, char *argv[]) 
 {
-int nthreads, i, tid;
-float total;
+  int nthreads, i, tid;
+  float total=0.0,suma=0.0;
 
+  
 /*** Spawn parallel region ***/
-#pragma omp parallel private(tid) 
+#pragma omp parallel private(tid) reduction(+:total)
   {
+    suma=0.0;
   /* Obtain thread number */
   tid = omp_get_thread_num();
   /* Only master thread does this */
@@ -26,16 +28,22 @@ float total;
     }
   printf("Thread %d is starting...\n",tid);
 
-  #pragma omp barrier
-
+    #pragma omp barrier
+  
   /* do some work */
-  total = 0.0;
-  #pragma omp parallel for schedule(dynamic,10)
-  for (i=0; i<1000000; i++) 
+  // total = 0.0;
+#pragma omp  for    schedule(dynamic,2) //	\
+  private(i)//				\
+  reduction(+:total)
+  for (i=0; i<1000000; i++) {
      total = total + i*1.0;
-
-  printf ("Thread %d is done! Total= %e\n",tid,total);
-
+     suma=suma+i*1.0;
+  }
+  printf ("Thread %d is done! Total= %f\n",tid,total);
+  //   #pragma omp barrier
+  //  printf ("Thread %d is done! Total= %f\n",tid,total);
+  //printf ("LA suma total es= %f\n",suma);
+  
   } /*** End of parallel region ***/
-  //   printf ("Thread %d is done! Total= %e\n",tid,total);
+     printf ("LA suma total es= %f\n",total);
 }
--- 2019-11-20-Taller/omp_mm-NEW.c
+++ 2019-11-20-Taller/omp_mm-NEW.c
@@ -11,16 +11,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NRA 62                 /* number of rows in matrix A */
-#define NCA 15                 /* number of columns in matrix A */
-#define NCB 7                  /* number of columns in matrix B */
+
+#define NRA 1000                /* number of rows in matrix A */
+#define NCA 1000                 /* number of columns in matrix A */
+#define NCB 1000                  /* number of columns in matrix B */
+#define N 1000
 
 int main (int argc, char *argv[]) 
 {
 int	tid, nthreads, i, j, k, chunk;
-double	a[NRA][NCA],           /* matrix A to be multiplied */
-	b[NCA][NCB],           /* matrix B to be multiplied */
-	c[NRA][NCB];           /* result matrix C */
+
+// double	a[NRA*NCA],           /* matrix A to be multiplied */
+//	b[NCA*NCB],           /* matrix B to be multiplied */
+//	c[NRA*NCB];           /* result matrix C */
+
+ double *a = new double [N*N];
+ double *b = new double [N*N];
+ double *c = new double [N*N];
 
 chunk = 10;                    /* set loop iteration chunk size */
 
@@ -31,22 +38,22 @@ chunk = 10;                    /* set loop iteration chunk size */
   if (tid == 0)
     {
     nthreads = omp_get_num_threads();
-    printf("Starting matrix multiple example with %d threads\n",nthreads);
-    printf("Initializing matrices...\n");
+    // printf("Starting matrix multiple example with %d threads\n",nthreads);
+    // printf("Initializing matrices...\n");
     }
   /*** Initialize matrices ***/
   #pragma omp for schedule (static, chunk) 
   for (i=0; i<NRA; i++)
     for (j=0; j<NCA; j++)
-      a[i][j]= i+j;
+      a[N*i+j]= i+j;
   #pragma omp for schedule (static, chunk)
   for (i=0; i<NCA; i++)
     for (j=0; j<NCB; j++)
-      b[i][j]= i*j;
+      b[N*i+j]= i*j;
   #pragma omp for schedule (static, chunk)
   for (i=0; i<NRA; i++)
     for (j=0; j<NCB; j++)
-      c[i][j]= 0;
+      c[N*i+j]= 0;
 
   /*** Do matrix multiply sharing iterations on outer loop ***/
   /*** Display who does which iterations for demonstration purposes ***/
@@ -54,24 +61,27 @@ chunk = 10;                    /* set loop iteration chunk size */
   #pragma omp for schedule (static, chunk)
   for (i=0; i<NRA; i++)    
     {
-    printf("Thread=%d did row=%d\n",tid,i);
+      // printf("Thread=%d did row=%d\n",tid,i);
     for(j=0; j<NCB; j++)       
       for (k=0; k<NCA; k++)
-        c[i][j] += a[i][k] * b[k][j];
+        c[N*i+j] += a[N*i+k] * b[N*k+j];
     }
   }   /*** End of parallel region ***/
 
 /*** Print results ***/
-  
+  /*
 printf("******************************************************\n");
 printf("Result Matrix:\n");
 for (i=0; i<NRA; i++)
   {
   for (j=0; j<NCB; j++) 
-    printf("%6.2f   ", c[i][j]);
+    printf("%6.2f   ", c[N*i+j]);
   printf("\n"); 
   }
 printf("******************************************************\n");
-printf ("Done.\n");
+printf ("Done.\n");*/
 
+  delete [] a;
+  delete [] b;
+  delete [] c;
 }
