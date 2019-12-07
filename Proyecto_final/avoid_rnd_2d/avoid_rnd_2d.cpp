8d293cc390f7300706472a2b7d40b0eb2c1b634c
Slackware Live User <live@darkstar.example.net>
2019-12-07 00:59:59 -0800

    El programa para camino aleatorio funciona con arreglos estaticos

--- Proyecto_final/avoid_rnd_2d/avoid_rnd_2d.cpp
+++ Proyecto_final/avoid_rnd_2d/avoid_rnd_2d.cpp
@@ -6,31 +6,31 @@
  
 int main()
 {
-  int N=1;//Repeticiones
+  int N=500;//Repeticiones
   
-   int Steps=10;
-   int dim=2;//Dimension del programa
+  int Steps=1000;
+  int dim=2;//Dimension del programa
+  int contador=0;//Cuenta los pasos que se van realizando
+  double x=0.0;
+  double random=0.0;
 
-   
-   double x=0.0;
-   double random=0.0;
-
-int Position[N*Steps*2];//Guarda la posicion de los puntos utilizados
- int XY[Steps+1][dim];
-  int X[N];
-  int Y[N];
+  int Position[N*Steps*2];//Guarda la posicion de los puntos utilizados
+  //int XY[Steps*dim+1];
+  int x1=0,x2=0;//Variables temporales
+  int X1[Steps*N];
+  int X2[Steps*N];
   int Avg[Steps]; //Valor promedio de la posicion
    
   // int *X= new int[N+1];
   // int *Y= new int[N+1];
   // int *Avg= new int[Steps+1]; //Valor promedio de la posicion
-  for(int i=0;i<=Steps;++i)
-    for(int j=0;j<dim;++j)
-      XY[i][j]=0;
+  // for(int i=0;i<=Steps;++i)
+  //   for(int j=0;j<dim;++j)
+  //     XY[i][j]=0;
   
-   for(int i=0;i<N;++i)
+   for(int i=0;i<Steps*N;++i)
      {
-       X[i]=0;  Y[i]=0;
+       X1[i]=0;  X2[i]=0;
      }
 
    for(int i=0;i<Steps;++i)
@@ -44,33 +44,37 @@ int Position[N*Steps*2];//Guarda la posicion de los puntos utilizados
  for(int i=0;i<N;++i)
    {
      // printf("%5d",n);
-     for(int j=0;j<Steps;++j){
+     for(int j=0;j<Steps-1;++j){
        
        random=dis(gen);
        if(random<0.25){
-	 X[i]+=1;
+	 x1+=1;
        }
        else if(random<0.5 ){
-	 Y[i]+=1;
+	 x2+=1;
        }
        else if(random<0.75){
-	 X[i]-=1;
+	 x1-=1;
        }
        else{
-	 Y[i]-=1;
+	 x2-=1;
        }
-       XY[j+1][0]=X[i]+XY[j][0];
-       XY[j+1][1]=Y[i]+XY[j][1];
-       X[i]=0;
-       Y[i]=0;
+       //XY[j+1][0]=X[i]+XY[j][0];
+       //XY[j+1][1]=Y[i]+XY[j][1];
+       X1[j+1+i*Steps]=x1+X1[j+i*Steps];
+       X2[j+1+i*Steps]=x2+X2[j+i*Steps];
+       x1=0;
+       x2=0;
      }
    }
 
- for(int i=0;i<=Steps;++i){
-   for(int j=0;j<dim;++j){
-      printf(" %3d ",XY[i][j]);
+ for(int i=0;i<Steps;++i){
+   for(int j=i;j<Steps*N;j+=Steps){
+     printf(" %4d %4d ",X1[j],X2[j]);
+     Avg[i]+=X1[j]*X1[j]+X2[j]*X2[j];
    }
-   printf("\n");
+   printf("%7.3f %5d \n",Avg[i]*1.0/N,contador);
+   contador++;
  }
 // std::cout << '\n';
 
